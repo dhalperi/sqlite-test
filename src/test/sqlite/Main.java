9c15c90486db74b43236feec99d82d8dd72b50b9
Daniel Halperin <dhalperi@cs.washington.edu>
2013-04-20 12:07:26 -0700

    main: pretty print
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- src/test/sqlite/Main.java
+++ src/test/sqlite/Main.java
@@ -12,6 +12,49 @@ import com.almworks.sqlite4java.SQLiteConnection;
  * 
  */
 public final class Main {
+  public static String prettyNanoTime(long nanoTime) {
+    StringBuilder sb = new StringBuilder();
+    /* Start in hours */;
+    long current = 1000L * 1000L * 1000L * 60L * 60L;
+    if (nanoTime >= current) {
+      long hours = nanoTime / (current);
+      sb.append(hours).append(" hours ");
+      nanoTime %= current;
+    }
+    /* Minutes */
+    current = current / 60L;
+    if (nanoTime >= current) {
+      long minutes = nanoTime / (current);
+      sb.append(minutes).append(" minutes ");
+      nanoTime %= current;
+    }
+    /* Seconds */
+    current = current / 60L;
+    if (nanoTime >= current) {
+      long seconds = nanoTime / (current);
+      sb.append(seconds).append(" seconds ");
+      nanoTime %= current;
+    }
+    /* Milliseconds */
+    current = current / 1000L;
+    if (nanoTime >= current) {
+      long msecs = nanoTime / (current);
+      sb.append(msecs).append(" ms ");
+      nanoTime %= current;
+    }
+    /* Microseconds */
+    current = current / 1000L;
+    if (nanoTime >= current) {
+      long usecs = nanoTime / (current);
+      sb.append(usecs).append(" us ");
+      nanoTime %= current;
+    }
+    if (nanoTime != 0) {
+      sb.append(nanoTime).append(" ns");
+    }
+    return sb.toString();
+  }
+
   /**
    * Run the main program.
    * 
@@ -23,10 +66,13 @@ public final class Main {
 
     Path tempFile = Files.createTempFile("temp", "db");
     tempFile.toFile().deleteOnExit();
+
+    long start = System.nanoTime();
     SQLiteConnection conn = new SQLiteConnection(tempFile.toFile());
     conn.open();
-
     conn.dispose();
+    System.err.println(prettyNanoTime(System.nanoTime() - start));
+
     System.exit(0);
   }
 
